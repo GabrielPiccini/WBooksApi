3105440434fd9bb1897b6342ff0ea275699f661f
Gabriel <gabriel.piccini@widergy.com>
2021-03-18 14:38:43 -0300

    Corrected user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
 end
