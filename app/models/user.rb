59428244de70ffaf3cf1adc781ed967750e63c06
Gabriel <gabriel.piccini@widergy.com>
2021-03-25 18:12:52 -0300

    Rent model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
+  has_many :rents
 end
