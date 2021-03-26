2c32b53d55f5b61aaed2906406d1c46114b5c678
Gabriel Piccini <gabriel.piccini@widergy.com>
2021-03-26 15:54:33 -0300

    Implemented tests for Rent and User models

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,4 +1,9 @@
 class Rent < ApplicationRecord
+  validates :user, presence: true
+  validates :book, presence: true
+  validates :from, presence: true
+  validates :to, presence: true
+
   belongs_to :user
   belongs_to :book
 end
--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe Rent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Rent, type: :model do
+  it do
+    should validate_presence_of(:user)
+  end
+
+  it do
+    should validate_presence_of(:book)
+  end
+
+  it do
+    should validate_presence_of(:from)
+  end
+  
+  it do
+    should validate_presence_of(:to)
+  end
+
+  context 'When rent is created' do
+    subject(:rent) do
+      create(:rent, user: create(:user), book: create(:book))
+    end
+    it do
+      is_expected.to be_valid
+    end
+  end
+
+  describe '#create' do
+    context 'When the user is nil' do
+      subject(:rent) do
+        build(:rent, user: nil)
+      end
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the book is nil' do
+      subject(:rent) do
+        build(:rent, book: nil)
+      end
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When from is nil' do
+      subject(:rent) do
+        build(:rent, from: nil)
+      end
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When to is nil' do
+      subject(:rent) do
+        build(:rent, to: nil)
+      end
+      it do
+        is_expected.to be_invalid
+      end
+    end
+  end
 end
