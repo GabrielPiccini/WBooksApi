49bc968cc8443914ae2f7ba9a0ca840e3e9079e8
Gabiel Piccini <gabriel.piccini@widergy.com>
2021-03-31 18:06:09 -0300

    Fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    
+
     HOSTNAME = ENV['HOSTNAME']
 
 
@@ -27,4 +27,4 @@ module Myapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-end
+ends
