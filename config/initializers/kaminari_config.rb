0640c696d125c8494035bc7c44575ca163b38aec
Gabiel Piccini <gabriel.piccini@widergy.com>
2021-04-08 11:43:53 -0300

    Change kaminari gem config

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.page_method_name = :per_page_kaminari
   # config.default_per_page = 25
   # config.max_per_page = nil
   # config.window = 4
