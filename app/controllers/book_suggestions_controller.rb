d3c5003811a92e1aa3e3432da3d40d4f5f33ed27
Gabiel Piccini <gabriel.piccini@widergy.com>
2021-04-09 15:21:12 -0300

    Correct rubocop offenses

--- app/controllers/book_suggestions_controller.rb
+++ app/controllers/book_suggestions_controller.rb
@@ -1,9 +1,8 @@
 class BookSuggestionsController < ApiController
-
   def create
     all_params = suggestions_params.merge(user: current_user)
     @book_suggestion = BookSuggestion.create(all_params)
-    return render json: @book_suggestion, status: :created if @book_suggestion.save 
+    return render json: @book_suggestion, status: :created if @book_suggestion.save
 
     render json: @book_suggestion.errors, status: :unprocessable_entity
   end
@@ -15,5 +14,4 @@ class BookSuggestionsController < ApiController
                                             :editorial, :price, :publisher,
                                             :year)
   end
-    
 end
